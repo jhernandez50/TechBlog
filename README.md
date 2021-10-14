86b9bbfd2bd8cda0293636df3439dd1769f54837
Oliver Atom <j_hernandez50@hotmail.com>
2021-10-14 13:00:04 -0500

    Second commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Model-View-Controller (MVC): Tech Blog
+
+# Description
+For this project I have created a Blog APP, users will be able to login, and post comments into the blog.
+
+## User Story
+
+```md
+AS A developer who writes about tech
+I WANT a CMS-style blog site
+SO THAT I can publish articles, blog posts, and my thoughts and opinions
+```
+
+# Screenshot
+![Tech Blog](/public/images/techblog.png)
+
+# Technologies
+* JavaScript
+* Node.js
+* Handlebars
+* dotenv
+* MySQL
+* Sequilize
+
+# Links
+[Repository: https://github.com/layc41/tech-blog](https://github.com/jhernandez50/TechBlog.git)
+
+[Deployed Site: ](/)
---
+++ public/javascript/comment.js
@@ -0,0 +1,27 @@
+// Java script to gather the info from the comment form
+
+const commentFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const postId = document.querySelector('input[name="post-id"]').value;
+    const body = document.querySelector('textarea[name="comment-body"]').value;
+  
+    if (body) {
+      await fetch('/api/comment', {
+        method: 'POST',
+        body: JSON.stringify({
+          postId,
+          body
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      document.location.reload();
+    }
+  };
+  
+  document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', commentFormHandler);
